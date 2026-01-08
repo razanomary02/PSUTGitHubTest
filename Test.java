2b11827ffa963f2314d0a8327c7e158b3a453e54
razanomary02 <razanomary02@gmail.com>
2026-01-08 18:18:54 +0300

    Add print statement with razan  name

--- Test.java
+++ Test.java
@@ -1,14 +1,3 @@
-public class Test
-{
-    public static void main (String[] args)
-    {
-        System.out.println ("Testing git at PSUT");
-        
-        Person p1= new Person ("Mary", 12);
-        System.out.println (p1.getName ());
-        
-        System.out.println ("Firas");
-        System.out.println ("Yaza");
-        System.out.println ("Lobo");
-    }
-}
+System.out.println("Razan Al-Omari");
+
+
